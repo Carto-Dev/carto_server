7ca525afc4603e2383cc178928f0fe050c6fcf13
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2021-11-23 15:23:53 +0530

    Carto User Repository

---
+++ src/main/java/com/carto/server/modules/user/CartoUserRepository.java
@@ -0,0 +1,7 @@
+package com.carto.server.modules.user;
+
+import com.carto.server.model.CartoUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartoUserRepository extends JpaRepository<CartoUser, Long> {
+}
