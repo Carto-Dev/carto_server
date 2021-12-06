f40500ba1aa9ec181ad978bbef9327991f8cd1c4
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2021-12-06 08:57:19 +0530

    Update UserServiceImpl.java

--- src/main/java/com/carto/server/modules/user/UserServiceImpl.java
+++ src/main/java/com/carto/server/modules/user/UserServiceImpl.java
@@ -1,6 +1,5 @@
 package com.carto.server.modules.user;
 
-import com.carto.server.annotation.LoggedInUser;
 import com.carto.server.dto.user.NewUserDto;
 import com.carto.server.dto.user.UpdateUserDto;
 import com.carto.server.model.CartoUser;
