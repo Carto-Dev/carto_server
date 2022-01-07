56150f5de213cb0affe70c1c83468fac4d4a2b9a
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-01-07 16:40:25 +0530

    DTO implementation for fetching products by user. Closes #31

--- src/main/java/com/carto/server/modules/product/ProductRepository.java
+++ src/main/java/com/carto/server/modules/product/ProductRepository.java
@@ -14,4 +14,6 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     Set<Product> findByOrderByCreatedAtDesc();
 
+    Set<Product> findProductsByUser(CartoUser cartoUser);
+
 }
