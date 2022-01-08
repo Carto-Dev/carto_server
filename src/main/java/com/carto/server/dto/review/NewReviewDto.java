3c9abdad482ff6a9b173792e14c0ade16da12d5c
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-01-08 10:33:32 +0530

    DTO implementation for creation of reviews. Closes #40

---
+++ src/main/java/com/carto/server/dto/review/NewReviewDto.java
@@ -0,0 +1,22 @@
+package com.carto.server.dto.review;
+
+import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Data
+public class NewReviewDto {
+
+    @NotNull(message = "Review is required")
+    @NotEmpty(message = "Review is required")
+    @Size(min = 5, message = "Review should have at least 5 characters.")
+    private String text;
+
+    @NotNull(message = "Stars is required")
+    @Range(min = 0, max = 5)
+    private Long stars;
+
+}
