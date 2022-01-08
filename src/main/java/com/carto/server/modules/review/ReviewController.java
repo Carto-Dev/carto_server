8193069a6ad6c8c4f6ece7b891905c62811cb6a7
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-01-08 11:01:28 +0530

    Controller implementation for updating of reviews. Closes #45

--- src/main/java/com/carto/server/modules/review/ReviewController.java
+++ src/main/java/com/carto/server/modules/review/ReviewController.java
@@ -2,15 +2,13 @@ package com.carto.server.modules.review;
 
 import com.carto.server.annotation.LoggedInUser;
 import com.carto.server.dto.review.NewReviewDto;
+import com.carto.server.dto.review.UpdateReviewDto;
 import com.carto.server.exception.NotFoundException;
 import com.carto.server.model.CartoUser;
 import com.carto.server.model.Review;
 import com.carto.server.modelDtos.ReviewDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -24,6 +22,16 @@ public class ReviewController {
     @PostMapping
     public ReviewDto createReview(@LoggedInUser CartoUser cartoUser, @Valid @RequestBody NewReviewDto newReviewDto) throws NotFoundException {
         Review savedReview = this.reviewService.createReview(cartoUser, newReviewDto);
+
+        ReviewDto reviewDto = new ReviewDto();
+        reviewDto.convertToDto(savedReview);
+
+        return reviewDto;
+    }
+
+    @PutMapping
+    public ReviewDto updateReview(@LoggedInUser CartoUser cartoUser, @Valid @RequestBody UpdateReviewDto updateReviewDto) throws NotFoundException {
+        Review savedReview = this.reviewService.updateReview(cartoUser, updateReviewDto);
 
         ReviewDto reviewDto = new ReviewDto();
         reviewDto.convertToDto(savedReview);
