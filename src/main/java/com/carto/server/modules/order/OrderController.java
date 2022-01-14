d2830d12c239e6abb4e2e3e98580b0a74b4cbe5d
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-01-14 08:35:31 +0530

    Controller Implementation for fetching orders for a particular user. Closes #56

--- src/main/java/com/carto/server/modules/order/OrderController.java
+++ src/main/java/com/carto/server/modules/order/OrderController.java
@@ -7,12 +7,11 @@ import com.carto.server.model.CartoUser;
 import com.carto.server.model.Order;
 import com.carto.server.modelDtos.OrderDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(path = "v1/order")
@@ -29,6 +28,18 @@ public class OrderController {
         orderDto.convertToDto(order);
 
         return orderDto;
+    }
+
+    @GetMapping
+    public Set<OrderDto> fetchOrdersByUser(@LoggedInUser CartoUser cartoUser) {
+        Set<Order> orders = this.orderService.fetchOrdersByUser(cartoUser);
+
+        return orders.stream().map(order -> {
+            OrderDto orderDto = new OrderDto();
+            orderDto.convertToDto(order);
+
+            return orderDto;
+        }).collect(Collectors.toSet());
     }
 
 }
