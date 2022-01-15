0a2a0724b4e710e9464be7aa81b29019d6faa5c2
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-01-15 11:15:56 +0530

    Initialize and Instantiate Algolia Search Indices. Closes #59

--- src/main/java/com/carto/server/modules/algolia/AlgoliaService.java
+++ src/main/java/com/carto/server/modules/algolia/AlgoliaService.java
@@ -2,16 +2,23 @@ package com.carto.server.modules.algolia;
 
 import com.algolia.search.DefaultSearchClient;
 import com.algolia.search.SearchClient;
+import com.algolia.search.SearchIndex;
+import com.carto.server.modelDtos.AlgoliaProductDto;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 
 @Service
+@Getter
+@Setter
 @Slf4j
 public class AlgoliaService {
 
-    SearchClient searchClient;
+    private SearchClient searchClient;
+    private SearchIndex<AlgoliaProductDto> productIndex;
 
     @PostConstruct
     public void initializeAlgolia() {
@@ -21,6 +28,11 @@ public class AlgoliaService {
         this.searchClient = DefaultSearchClient.create(algoliaApplicationId, algoliaAdminKey);
 
         log.info("Algolia Search Client Initialized");
+
+        this.productIndex = this.searchClient.initIndex("products", AlgoliaProductDto.class);
+
+        log.info("Algolia Search Index Initialized");
+
     }
 
 }
