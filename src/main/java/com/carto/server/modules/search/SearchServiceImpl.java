db3e7d57516c5a796618850e5c96aeea3ac67af5
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-01-15 11:49:49 +0530

    Log algolia actions in console

--- src/main/java/com/carto/server/modules/search/SearchServiceImpl.java
+++ src/main/java/com/carto/server/modules/search/SearchServiceImpl.java
@@ -4,6 +4,7 @@ import com.carto.server.model.Product;
 import com.carto.server.modelDtos.AlgoliaProductDto;
 import com.carto.server.modules.algolia.AlgoliaService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -11,6 +12,7 @@ import javax.transaction.Transactional;
 @Service
 @Transactional
 @RequiredArgsConstructor
+@Slf4j
 public class SearchServiceImpl implements SearchService {
 
     private final AlgoliaService algoliaService;
@@ -24,10 +26,14 @@ public class SearchServiceImpl implements SearchService {
 
         this.algoliaService.getProductIndex().saveObject(algoliaProductDto);
 
+        log.info("Added/Updated Product:" + product.getId().toString() + " to Algolia");
+
     }
 
     @Override
     public void deleteProduct(Product product) {
         this.algoliaService.getProductIndex().deleteObject(product.getId().toString());
+        log.info("Deleted Product:" + product.getId().toString() + " from Algolia");
+
     }
 }
