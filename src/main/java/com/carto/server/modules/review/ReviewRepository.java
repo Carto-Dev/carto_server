e7f15b5a26447c4240addac427d8ee6158958a1a
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-02-12 10:48:51 +0530

    Service Implementation for fetching reviews by logged in user. Closes #76

--- src/main/java/com/carto/server/modules/review/ReviewRepository.java
+++ src/main/java/com/carto/server/modules/review/ReviewRepository.java
@@ -6,9 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.Set;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    Optional<Set<Review>> findReviewsByUser(CartoUser cartoUser);
 
     Optional<Review> findByIdAndUser(Long id, CartoUser cartoUser);
 
