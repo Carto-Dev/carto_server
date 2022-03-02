029f633af2b98b36df4dfd96e20d8cbcf1194424
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-03-02 10:44:43 +0530

    Update ProductCategoryRepository.java

--- src/main/java/com/carto/server/modules/product/ProductCategoryRepository.java
+++ src/main/java/com/carto/server/modules/product/ProductCategoryRepository.java
@@ -4,9 +4,13 @@ import com.carto.server.model.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Set;
+
 @Repository
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 
     ProductCategory findByKey(String key);
+
+    Set<ProductCategory> findAllByKeyIn(Set<String> key);
 
 }
